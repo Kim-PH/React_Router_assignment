bb3e05c95b3949196079a9fcef84d011ceec6839
Kim <59347764+Kim-PH@users.noreply.github.com>
2021-03-22 01:32:41 -0400

    React Router Test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink
+} from "react-router-dom";
+import Home from './pages/Home.js';
+import About from './pages/About.js';
+import Projects from './pages/Projects.js';
+import Contact from './pages/Contact.js';
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <NavLink to="/home" activeClassName="selected">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about" activeClassName="selected">About</NavLink>
+            </li>
+            <li>
+              <NavLink to="/projects" activeClassName="selected">Projects</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact" activeClassName="selected">Contact</NavLink>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          <Route path="/contact">
+            < Contact/>
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
---
+++ src/pages/Projects.js
@@ -0,0 +1,22 @@
+import '../App.css';
+import './Home.css';
+
+
+export default function Projects() {
+    return (
+
+        <div className="App">
+            <header className="App-header">
+                <h1>Projects</h1>
+                <p>Click the link below to see my projects</p>
+                <a className="App-link" href="https://kimpham09.myportfolio.com/work" target="_blank" rel="noopener noreferrer">Projects</a>
+            </header>
+
+            <footer className="App-footer">
+                © Copyright
+            </footer>
+        </div>
+        
+    );
+}
+
